d2eb16031a92c3f318abd368fe065077b064395c
yosepjeon <enekelx1@naver.com>
2020-01-17 19:15:54 +0900

    spring rest api 개발 프로젝트 생성

---
+++ src/main/java/com/yosep/restful/YosepSpringRestfulProjectApplication.java
@@ -0,0 +1,13 @@
+package com.yosep.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YosepSpringRestfulProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YosepSpringRestfulProjectApplication.class, args);
+	}
+
+}
