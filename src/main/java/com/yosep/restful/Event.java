c0fdc05602b37a9a6235f8b1fde8be6c4a866eab
yosepjeon <enekelx1@naver.com>
2020-01-18 20:31:21 +0900

    event domain 구현

---
+++ src/main/java/com/yosep/restful/Event.java
@@ -0,0 +1,33 @@
+package com.yosep.restful;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of="id")
+public class Event {
+	private Integer id;
+	private String name;
+	private String description;
+	private LocalDateTime beginEnrollmentDateTime;
+	private LocalDateTime closeEnrollmentsDateTime;
+	private LocalDateTime beginEventDateTime;
+	private LocalDateTime endEventDateTime;
+	private String location; // (optional) 이게 없으면 온라인 모임
+	private int basePrice; // (optional)
+	private int maxPrice; // (optional)
+	private int limitOfEnrollment;
+	private boolean offline;
+	private boolean free;
+	private EventStatus eventStatus;
+}
---
+++ src/main/java/com/yosep/restful/EventStatus.java
@@ -0,0 +1,5 @@
+package com.yosep.restful;
+
+public enum EventStatus {
+	DRAFT, PUBLISHED, BEGAN_ENROLLMENT;
+}
